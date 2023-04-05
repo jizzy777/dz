03baccb905ce2cf7e58b11a92080d6c37a5ec808
jizzy777 <jizzy777@ya.ru>
2023-04-05 17:10:09 +0500

    28

---
+++ 28.1/settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "C:/Users/grafg/Desktop/Python/chromedriver_win32/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'jizzy777@ya.ru'
+    valid_password = 'Jizzy777'
+    valid_email_reg = 'jizzy777@mail.ru'
+    invalid_email = 'gds3455sas@mail.ru'
+    invalid_password = 'invdPasss'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Jion'
+    last_name = 'Test'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'й'
+    latin_generate_string = 'ffoott'
+    chinese_chars = '形声字/形聲字象形字'
+    special_chars = '!@#$%^&*()_+=/{}'
+    numbers = 274
+    empty = ''
+    passw1 = 'zXc12345'
+    passw2 = 'zXc12345vBn09Qw'
+    passw3 = 'ZaQwsX987654321Ujb42'
